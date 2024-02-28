e2560217669195e11a670dbe21c7ab2a0136b956
eduneves15 <132684609+eduneves15@users.noreply.github.com>
2024-02-28 09:46:04 -0300

    calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,58 @@
+function somar(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const Totalsoma = num1 + num2;
+
+    console.log(`A soma ${num1} + ${num2} =${Totalsoma}`);
+}
+
+function sub(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const Totalsub = num1 - num2;
+
+    console.log(`A subtração ${num1} - ${num2} =${Totalsub}`);
+}
+
+function mul(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const Totalmul = num1 * num2;
+
+    console.log(`A multiplicação ${num1} * ${num2} =${Totalmul}`);
+}
+
+function div(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const Totaldiv = num1 / num2;
+
+    console.log(`A divisão ${num1} / ${num2} =${Totaldiv}`);
+}
+
+somar();
+sub();
+mul();
+div();
+
+
+
+function somar2(num1,num2){
+    const Total = num1 + num2;
+
+    console.log(`A soma ${num1} + ${num2} = ${Totalsoma}`);
+}
+
+function somar3(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const Total = num1 + num2;
+
+    console.log(`A soma ${num1} + ${num2} = ${Totalsoma}`);
+}
+
