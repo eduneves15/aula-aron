07246a9ddfdc5954eccc4bb3e802d60f91ab875b
eduneves15 <132684609+eduneves15@users.noreply.github.com>
2024-02-29 09:38:03 -0300

    login.js

--- login.js
+++ login.js
@@ -17,23 +17,49 @@
 
 //Email | Senha | Confirmar Senha | Data de Nascimento | Número de Telefone
 
-function cadastro(email, senha, Confirmar_senha, data_nascimento, numero_telefone){
-    if (email === "eduardo@gmail.com" && senha === "edulindo" && Confirmar_senha === "edulindo" && data_nascimento === "22/02/2007" && numero_telefone === "45999246348"){
-        return "Status 200 (Tudo Certo)"
-    }else{
-        return "Status 400 (Erro)"
-    }
+
+
+
+
+//function cadastro(email, senha, Confirmar_senha, data_nascimento, numero_telefone){
+   // if (email === "eduardo@gmail.com" && senha === "edulindo" && Confirmar_senha === "edulindo" && data_nascimento === "22/02/2007" && numero_telefone === "45999246348"){
+    //    return "Status 200 (Tudo Certo)"
+   // }else{
+     //   return "Status 400 (Erro)"
+    //}
    
+//}
+//const email = prompt("Digite email:");
+//const senha = prompt("Digite a senha:");
+//const Confirmar_senha = prompt("Confirme a senha:");
+//const data_nascimento = prompt("Data de Nascimento:");
+//const numero_telefone = prompt("Número de Telefone");
+
+//const response = cadastro(email, senha, Confirmar_senha, data_nascimento, numero_telefone );
+
+//console.log(response);
+
+
+
+
+function registro(email, senha, Confirmar_senha, data, telefone){
+    if(email === "" || senha === "" || Confirmar_senha === "" || data === "" || telefone === ""){
+        return "Status 400(Todos os campos são necessários)"
+    }else{
+        if(senha != Confirmar_senha){
+            return "Status 400 (As senhas devem ser iguais)"
+        }else{
+            return "Status 200 (ok)"
+        }
+    }
 }
 const email = prompt("Digite email:");
 const senha = prompt("Digite a senha:");
 const Confirmar_senha = prompt("Confirme a senha:");
-const data_nascimento = prompt("Data de Nascimento:");
-const numero_telefone = prompt("Número de Telefone");
+const data = prompt("Data de Nascimento:");
+const telefone = prompt("Número de Telefone");
 
-const response = cadastro(email, senha, Confirmar_senha, data_nascimento, numero_telefone );
+const response = cadastro(email, senha, Confirmar_senha, data, telefone );
 
 console.log(response);
 
-
-   
