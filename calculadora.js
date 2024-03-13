099fbb516827b80111edefe774d2955675098f8f
eduneves15 <132684609+eduneves15@users.noreply.github.com>
2024-03-13 08:43:01 -0300

    calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,53 +1,28 @@
 function somar(){
+    const opera = prompt("Escolha sua operção")
     const num1 = parseInt(prompt("De um numero : "));
     const num2 = parseInt(prompt("De outro numero: "));
 
+    if ("soma" == opera){
     const totalsoma = num1 + num2;
     console.log(`A soma ${num1} + ${num2} = ${totalsoma}`);
-}
-
-function sub(){
-    const num1 = parseInt(prompt("De um numero : "));
-    const num2 = parseInt(prompt("De outro numero: "));
-
-
+    }
+    else if("subtracao" == opera){
     const totalsub = num1 - num2;
     console.log(`A subtração ${num1} - ${num2} = ${totalsub}`);
-}
-
-function mul(){
-    const num1 = parseInt(prompt("De um numero: "));
-    const num2 = parseInt(prompt("De outro numero: "));
-
-
+    }
+    else if("multiplicacao" == opera){
     const totalmul = num1 * num2;
     console.log (`A multiplição ${num1} x ${num2} = ${totalmul}`);
-    
-}
-
-function div(){
-    const num1 = parseInt(prompt("De um numero: "));
-    const num2 = parseInt(prompt("De outro numero: "));
-
-
+    }
+    else if("divisao" == opera){
     const totaldiv = num1 / num2;
     console.log (`A divisão ${num1} / ${num2} = ${totaldiv}`);
-    
+    }
 }
 
 somar();
 sub();
 mul();
 div();
-
-function somar2(num1, num2){
-    const total = num1 + num2;
-
-    console.log(`A soma ${num1} + ${num2} = ${total}`);
-}
-
-function somar3(){
-    const num1 = 10;
-    const num2 = 20;
-}
 
